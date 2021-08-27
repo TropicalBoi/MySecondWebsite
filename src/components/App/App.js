df4c2a156c2228869c6a9faa2b3649bd2dff8e66
TropicalBoi <tewprai.bualoi@gmail.com>
2021-08-27 18:58:09 +0700

    firstUpload

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import logo from '../../logo.svg';
+import logoScale from '../../logoScale.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logoScale} className="Logo-scale" alt="logo" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
