d6566d892b8d1b699a7fd7260632f82da527f5e4
TropicalBoi <tewprai.bualoi@gmail.com>
2021-08-29 02:20:09 +0700

    test flex box

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,23 @@
 import logo from '../../logo.svg';
 import logoScale from '../../logoScale.svg';
+import zing from './ZingHome.jpg';
+import asyou from './AsYouGrowHome.jpg'
+import iwill from './IWillHome.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="About">ABOUT</p>
         <img src={logoScale} className="Logo-scale" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div className="App-body">
+        <img src={zing} className="Zing" alt="zing" />
+        <img src={asyou} className="AsYou" alt="as-you-grow-older-but-never-wiser" />
+        <img src={iwill} className="IWill" alt="i-will-always-think-of-you-fondly" />
+      </div>
     </div>
   );
 }
