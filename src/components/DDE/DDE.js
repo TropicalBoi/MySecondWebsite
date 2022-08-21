c0d105cfb48a67a0459ea637a6fe8617fe94b569
TropicalBoi <tewprai.bualoi@gmail.com>
2022-08-22 03:49:10 +0700

    fix build bug

--- src/components/DDE/DDE.js
+++ src/components/DDE/DDE.js
@@ -2,29 +2,28 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 import "./DDE.css";
 import DDEtorch from "./torch.gif";
-import { css } from "@emotion/css";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const DDE = () => {
   return (
-    // <Link to="/TropicalDeepDungeon">
-    <div className="DDE">
-      <img src={DDEtorch} className="Torch1" alt="Deep Dungeon Entrance" />
-      <Typewriter
-        options={{
-          strings: [
-            "You’ve reached the Bottom!",
-            "Next would be the Deep Dungeon.",
-            "Coming very soon!",
-            "<3<3<3",
-          ],
-          autoStart: true,
-          loop: true,
-        }}
-      />
-      <img src={DDEtorch} className="Torch2" alt="Deep Dungeon Entrance" />
-    </div>
-    // </Link>
+    <Link to="/TropicalDeepDungeon">
+      <div className="DDE">
+        <img src={DDEtorch} className="Torch1" alt="Deep Dungeon Entrance" />
+        <Typewriter
+          options={{
+            strings: [
+              "You’ve reached the Bottom!",
+              "Next would be the Deep Dungeon.",
+              "Click to enter!",
+              "<3<3<3",
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+        <img src={DDEtorch} className="Torch2" alt="Deep Dungeon Entrance" />
+      </div>
+    </Link>
   );
 };
 
