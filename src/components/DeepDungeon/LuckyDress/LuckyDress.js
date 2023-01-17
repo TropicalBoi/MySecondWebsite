5a6c4a5d919b97c3ac0377965af17e5d594a0c22
TropicalBoi <tewprai.bualoi@gmail.com>
2023-01-17 20:16:48 +0700

    fix mobile responsive

--- src/components/DeepDungeon/LuckyDress/LuckyDress.js
+++ src/components/DeepDungeon/LuckyDress/LuckyDress.js
@@ -36,6 +36,15 @@ const LuckyDress = () => {
     fetch();
   });
 
+  useEffect(() => {
+    const documentHeight = () => {
+      const doc = document.documentElement;
+      doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
+    };
+    window.addEventListener("resize", documentHeight);
+    documentHeight();
+  });
+
   return (
     <div className="luckyDress">
       <div className="luckyMainBody">
